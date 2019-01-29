a96cb79e688d6d9fc969a9bb99a07bc8a3b48316
Paul Konstantin Gerke <paulkgerke@craftware.info>
2019-01-29 19:53:16 +0100

    Add license and finished up the setup.py file

--- setup.py
+++ setup.py
@@ -2,8 +2,11 @@ from distutils.core import setup
 
 setup(
     name='pipetime',
+    description='Tool to measure processing timings',
     version='1.0',
     author="Paul Konstantin Gerke",
     author_email="paul.gerke@radboudumc.nl",
-    requires=["bokeh (>=1.0.4)"])
+    requires=["bokeh (>=1.0.4)"],
+    license="MIT",
+)
 
